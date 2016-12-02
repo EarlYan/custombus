9117d941e00697271c10fcae2a8a3869a78df018
yanjiabei <ysuperpaul@gmail.com>
2016-12-02 15:00:15 +0800

    base bean

---
+++ src/com/dzbs/www/bean/common/Comment.java
@@ -0,0 +1,46 @@
+package com.dzbs.www.bean.common;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ * @author EarlYan
+ * 评论信息
+ */
+@Entity
+@Table(name="t_comment")
+public class Comment extends BasePO{
+
+	//评论者id
+	private int member_id;
+	
+	//被评论者id
+	private int commented_id;
+	
+	//评论内容
+	private String content;
+
+	public int getMember_id() {
+		return member_id;
+	}
+
+	public void setMember_id(int member_id) {
+		this.member_id = member_id;
+	}
+
+	public int getCommented_id() {
+		return commented_id;
+	}
+
+	public void setCommented_id(int commented_id) {
+		this.commented_id = commented_id;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+}
